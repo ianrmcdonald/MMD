3e83584f52ac49c986f4c5256a9198e3d669fea0
ianrmcdonald <ianrmcdonald@gmail.com>
2018-01-17 18:29:21 -0800

    Login should work now.

--- shor mccarty.R
+++ shor mccarty.R
@@ -14,7 +14,8 @@ npat_master <- npat_june_2015 %>% select(name, party, st, member_id, np_score)
 leg_counts <- read_csv("district numbers.csv")
 
 st_list <-  leg_counts %>% 
-  filter(double == TRUE) %>%  ##
+  filter(double == TRUE) %>%  ###
+        
   select(stcd) 
 
 st_list <- map_chr(st_list[[1]], as.character) #
